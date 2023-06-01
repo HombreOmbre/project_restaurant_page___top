a8aec07ee1f27849cf17439706f1f4e8a949aa86
Ombre <marcin.obrebowski@outlook.com>
2023-06-01 23:06:13 +0200

    Make function which create homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Hello, after webpack config');
+import header from './header.js';
+import footer from './footer.js';
+import main from './mainContainer.js';
+
+function createHomePage() {
+    let arrWithModules = [];
+    const container = document.querySelector('#content');
+    
+    arrWithModules = [
+        header(),
+        main(),
+        footer(),
+    ];
+
+    for (let i = 0; i < arrWithModules.length; i++) {
+        container.innerHTML += arrWithModules[i];
+    };
+
+}
+
+window.addEventListener('load', createHomePage);
